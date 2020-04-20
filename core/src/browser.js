e4ae45dceaf5722dd1583bece1c0cf3354edb9c0
Zach Vacura <zach@hackzzila.com>
2020-04-20 16:05:07 -0500

    Adds joining capability (#5)
    * add join method, rework connection method to work with latest signaller, add encryption support
    
    * fix eslint, run tests on ubuntu
    
    * fix extra space

--- core/src/browser.js
+++ core/src/browser.js
@@ -1,12 +1,14 @@
 module.exports = async () => {
-  const core = await import('../pkg-browser');
+  const core = await import('../pkg-browser'); // eslint-disable-line import/no-unresolved
 
   return {
-    Client: require('./Client')(WebSocket, undefined),
+    Client: require('./Client')(WebSocket, undefined, core, require('./KeyPair')(core)),
     DB: require('./DB'),
     Field: require('./Field'),
+    KeyPair: require('./KeyPair')(core),
+    PublicKey: require('./PublicKey')(core),
     Secret: require('./Secret'),
     Section: require('./Section'),
-    randomString: core.random_string,
+    randomString: core.randomString,
   };
 }
